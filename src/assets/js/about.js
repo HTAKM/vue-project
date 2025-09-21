d439a9feb1d9a0eb15bf71cf87075fe40c436264
HU/HTAKM <159533213+HTAKM@users.noreply.github.com>
2025-09-21 21:48:31 +0800

    Move some designs from old website into about page + Separate about page Vue into multiple files

---
+++ src/assets/js/about.js
@@ -0,0 +1,28 @@
+import Segment from '@/components/Segment.vue';
+import gradeFile from '@/assets/course-and-grade.csv';
+
+export default {
+  name: 'About',
+  components: {
+    Segment
+  },
+  props: {
+    isDark: Boolean
+  },
+  data() {
+    return {
+      isShowing: 'Contact',
+      GPAValidGrade: ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F']
+    };
+  },
+  computed: {
+    showGrades() {
+      return gradeFile.filter((row) => row.Grade && this.GPAValidGrade.includes(row.Grade));
+    }
+  },
+  methods: {
+    changeShowing(tab) {
+      this.isShowing = tab;
+    }
+  }
+};
--- src/view/About.vue
+++ src/view/About.vue
@@ -1,34 +1,8 @@
-<script>
-import { ref } from 'vue';
-import Session from '@/components/Session.vue';
-import gradeFile from '@/assets/course-and-grade.csv';
-
-export default {
-  name: 'About',
-  components: {
-    Session
-  },
-  props: {
-    isDark: Boolean
-  },
-  data() {
-    return {
-      isShowing: 'Contact',
-      GPAValidGrade: ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F']
-    };
-  },
-  computed: {
-    showGrades() {
-      return gradeFile.filter((row) => row.Grade && this.GPAValidGrade.includes(row.Grade));
-    }
-  },
-  methods: {
-    changeShowing(tab) {
-      this.isShowing = tab;
-    }
-  }
-};
+<script src="@/assets/js/about.js">
 </script>
-<template src="../assets/html/about.html" />
-<style scoped style="scss">
+
+<template src="@/assets/html/about.html">
+</template>
+
+<style scoped lang="scss">
 </style>
